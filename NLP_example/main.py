00fa01a8941a6dcae04b11c694b8baab8b460918
Kevin Turcios <turcioskevinr@gmail.com>
2025-09-13 18:10:06 -0700

    add NLP example

---
+++ NLP_example/main.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download('averaged_perceptron_tagger_eng')
+
+sentence = """At eight o'clock on Thursday morning
+... Arthur didn't feel very good."""
+
+tokens = nltk.word_tokenize(sentence)
+print(tokens)
+
+tagged = nltk.pos_tag(tokens)
+print(tagged[0:6])
