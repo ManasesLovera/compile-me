318bafb714080d6e10b9ca98790d983488092585
Kevin Turcios <turcioskevinr@gmail.com>
2025-09-15 00:03:18 -0700

    add NLP example

--- NLP_example/main.py
+++ NLP_example/main.py
@@ -1,7 +1,8 @@
 # examples derived from https://www.nltk.org
 
 import nltk
-nltk.download('averaged_perceptron_tagger_eng')
+
+nltk.download("averaged_perceptron_tagger_eng")
 
 sentence = """At eight o'clock on Thursday morning
 ... Arthur didn't feel very good."""
@@ -11,3 +12,21 @@ print(tokens)
 
 tagged = nltk.pos_tag(tokens)
 print(tagged[0:6])
+
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+
+for token in doc:
+    print(
+        token.text,
+        token.lemma_,
+        token.pos_,
+        token.tag_,
+        token.dep_,
+        token.shape_,
+        token.is_alpha,
+        token.is_stop,
+    )
